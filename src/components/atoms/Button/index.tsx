0e6e176c94f575e57dbff80c6d72ccf5568ba221
jadekang0611 <56938630+jadekang0611@users.noreply.github.com>
2023-05-17 23:27:58 -0700

    fix: removed focus styling from button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -4,7 +4,7 @@ const Button = ({ children }: ButtonProps) => {
   return (
     <button
       type="submit"
-      className="mt-4 px-6 py-2 text-white bg-[#19D1B2] rounded-sm hover:bg-[#14A38B] focus:outline-none focus:bg-blue-600 w-full"
+      className="mt-4 px-6 py-2 text-white bg-[#19D1B2] rounded-sm hover:bg-[#14A38B] focus:outline-none w-full"
     >
       {children}
     </button>
